2464140b8082482cd8f9c86ff776123c1dd6b239
X Yan <ansonyxr@gmail.com>
2021-04-08 15:52:29 -0700

    remove test code

--- server.js
+++ server.js
@@ -25,7 +25,6 @@ mongoose.connect(connection_url, {
 
 // api endpoints
 app.get("/", (req, res) => res.status(200).send("Hello world"));
-app.get("/v1/posts", (req, res) => res.status(200).send(Data));
 
 app.get("/v2/posts", (req, res) => {
   Videos.find((err, data) => {
